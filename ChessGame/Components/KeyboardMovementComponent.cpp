c86d5a29549fc5cf77673546f051e90c08528b45
VLADBORODAY9C84\vladboroday <qwe>
2023-03-21 23:54:46 +0200

    added raylib as a submodule

---
+++ ChessGame/Components/KeyboardMovementComponent.cpp
@@ -0,0 +1,5 @@
+﻿#include "KeyboardMovementComponent.h"
+
+void KeyboardMovementComponent::OnUpdate(float deltaTime)
+{
+}
---
+++ ChessGame/Components/KeyboardMovementComponent.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include "Components/GameObjectComponent.h"
+
+class KeyboardMovementComponent : GameObjectComponent
+{
+public:
+    void OnUpdate(float deltaTime) override;
+};
